7fe804b29bf41920584b23d58ee32f8be0f26589
Vương Ngọc Quân <quanvuongngoc98@gmail.com>
2020-11-29 14:13:27 +0700

    info UI

--- cvapp/src/features/information/UserInfo.js
+++ cvapp/src/features/information/UserInfo.js
@@ -1,4 +1,4 @@
-import { CameraOutlined, MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
+import { CameraOutlined, MinusCircleOutlined, PlusCircleOutlined } from '@ant-design/icons'
 import { Button, Radio, Tabs, Form, Input, DatePicker, InputNumber } from 'antd'
 import React, { useState } from 'react'
 import TagGroup from './TagGroup'
@@ -59,7 +59,7 @@ export function UserInfo() {
               </Form.Item>
 
               <Form.Item label='Ngày sinh' name='dob'>
-                <DatePicker locale={VN} />
+                <DatePicker locale={VN} format={'DD-MM-YYYY'} />
               </Form.Item>
 
               <Form.Item label='Địa chỉ' name='address'>
@@ -118,27 +118,18 @@ export function UserInfo() {
                   <>
                     <Form.Item {...layout} label={'Mạng xã hội'} required={false}>
                       <Form.Item noStyle>
-                        <Input placeholder='Facebook' />
+                        <Input placeholder='Facebook, LinkedIn...' style={{ width: 'calc(100% - 40px)' }} />
                       </Form.Item>
-                    </Form.Item>
-                    <Form.Item {...tailLayout} label={''} required={false}>
-                      <Form.Item noStyle>
-                        <Input placeholder='LinkedIn' />
-                      </Form.Item>
+                      <PlusCircleOutlined className='dynamic-delete-button' onClick={() => add()} />
                     </Form.Item>
                     {fields.map((field) => (
                       <Form.Item {...tailLayout} label={''} required={false} key={field.key}>
                         <Form.Item {...field} noStyle>
-                          <Input placeholder='Khác...' style={{ width: 'calc(100% - 40px)' }} />
+                          <Input style={{ width: 'calc(100% - 40px)' }} />
                         </Form.Item>
                         <MinusCircleOutlined className='dynamic-delete-button' onClick={() => remove(field.name)} />
                       </Form.Item>
                     ))}
-                    <Form.Item {...tailLayout}>
-                      <Button type='dashed' onClick={() => add()} icon={<PlusOutlined />}>
-                        Thêm link
-                      </Button>
-                    </Form.Item>
                   </>
                 )}
               </Form.List>
@@ -171,6 +162,232 @@ export function UserInfo() {
             <Form {...layout}>
               <Form.Item label='Thông tin thêm' name='additional'>
                 <Input.TextArea />
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                <Button type='primary' htmlType='submit'>
+                  Lưu
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab='Profile' key='5'>
+          <div style={{ padding: '16px 24px 16px 0' }}>
+            <Form {...layout}>
+              <Form.Item label='Vị trí ứng tuyển' name='position'>
+                <Input />
+              </Form.Item>
+
+              <Form.Item label='Profile' name='profile'>
+                <Input.TextArea />
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                <Button type='primary' htmlType='submit'>
+                  Lưu
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab='Xuất bản, thuyết trình' key='6'>
+          <div style={{ padding: '16px 24px 16px 0' }}>
+            <Tabs defaultActiveKey='61' centered>
+              <Tabs.TabPane tab='Sách' key='61'>
+                <Form {...layout}>
+                  <Form.List name='authors'>
+                    {(fields, { add, remove }) => (
+                      <>
+                        <Form.Item {...layout} label={'Tác giả'} required={false}>
+                          <Form.Item noStyle>
+                            <Input style={{ width: 'calc(100% - 40px)' }} />
+                          </Form.Item>
+                          <PlusCircleOutlined className='dynamic-delete-button' onClick={() => add()} />
+                        </Form.Item>
+                        {fields.map((field) => (
+                          <Form.Item {...tailLayout} label={''} required={false} key={field.key}>
+                            <Form.Item {...field} noStyle>
+                              <Input style={{ width: 'calc(100% - 40px)' }} />
+                            </Form.Item>
+                            <MinusCircleOutlined className='dynamic-delete-button' onClick={() => remove(field.name)} />
+                          </Form.Item>
+                        ))}
+                      </>
+                    )}
+                  </Form.List>
+
+                  <Form.Item label='Tiêu đề' name='title'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Nhà xuất bản' name='publisher'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Năm xuất bản' name='year'>
+                    <DatePicker locale={VN} picker='year' />
+                  </Form.Item>
+
+                  <Form.Item label='Nơi xuất bản' name='location'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Định dạng' name='format'>
+                    <Radio.Group buttonStyle='solid'>
+                      <Radio.Button value='APA'>APA</Radio.Button>
+                      <Radio.Button value='MLA'>MLA</Radio.Button>
+                    </Radio.Group>
+                  </Form.Item>
+
+                  <Form.Item {...tailLayout}>
+                    <Button type='primary' htmlType='submit'>
+                      Lưu
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab='Tạp chí' key='62'>
+                <Form {...layout}>
+                  <Form.List name='authors'>
+                    {(fields, { add, remove }) => (
+                      <>
+                        <Form.Item {...layout} label={'Tác giả'} required={false}>
+                          <Form.Item noStyle>
+                            <Input style={{ width: 'calc(100% - 40px)' }} />
+                          </Form.Item>
+                          <PlusCircleOutlined className='dynamic-delete-button' onClick={() => add()} />
+                        </Form.Item>
+                        {fields.map((field) => (
+                          <Form.Item {...tailLayout} label={''} required={false} key={field.key}>
+                            <Form.Item {...field} noStyle>
+                              <Input style={{ width: 'calc(100% - 40px)' }} />
+                            </Form.Item>
+                            <MinusCircleOutlined className='dynamic-delete-button' onClick={() => remove(field.name)} />
+                          </Form.Item>
+                        ))}
+                      </>
+                    )}
+                  </Form.List>
+
+                  <Form.Item label='Tiêu đề' name='title'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Tên tạp chí' name='journal'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Năm xuất bản' name='year'>
+                    <DatePicker locale={VN} picker='year' />
+                  </Form.Item>
+
+                  <Form.Item label='Tập, Kỳ'>
+                    <InputNumber min={0} name='volume' placeholder='Tập' />
+                    <InputNumber min={0} name='issue' placeholder='Kỳ' style={{ marginLeft: 8 }} />
+                  </Form.Item>
+
+                  <Form.Item label='Định dạng' name='format'>
+                    <Radio.Group buttonStyle='solid'>
+                      <Radio.Button value='APA'>APA</Radio.Button>
+                      <Radio.Button value='MLA'>MLA</Radio.Button>
+                    </Radio.Group>
+                  </Form.Item>
+
+                  <Form.Item {...tailLayout}>
+                    <Button type='primary' htmlType='submit'>
+                      Lưu
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab='Bài thuyết trình' key='63'>
+                <Form {...layout}>
+                  <Form.Item label='Tiêu đề' name='title'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Tên hội nghị' name='conference'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Nơi diễn ra' name='location'>
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item label='Năm diễn ra' name='year'>
+                    <DatePicker locale={VN} picker='year' />
+                  </Form.Item>
+
+                  <Form.Item {...tailLayout}>
+                    <Button type='primary' htmlType='submit'>
+                      Lưu
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </Tabs.TabPane>
+            </Tabs>
+          </div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab='Học bổng, tài trợ' key='7'>
+          <div style={{ padding: '16px 24px 16px 0' }}>
+            <Form {...layout}>
+              <Form.Item label='Học bổng' name='scholarship'>
+                <Input />
+              </Form.Item>
+
+              <Form.Item label='Trao bởi' name='from'>
+                <Input />
+              </Form.Item>
+
+              <Form.Item label='Năm nhận' name='year'>
+                <DatePicker locale={VN} picker='year' />
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                <Button type='primary' htmlType='submit'>
+                  Lưu
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab='Giải thưởng, danh hiệu' key='8'>
+          <div style={{ padding: '16px 24px 16px 0' }}>
+            <Form {...layout}>
+              <Form.Item label='Giải thưởng' name='award'>
+                <Input />
+              </Form.Item>
+
+              <Form.Item label='Trao bởi' name='from'>
+                <Input />
+              </Form.Item>
+
+              <Form.Item label='Năm nhận' name='year'>
+                <DatePicker locale={VN} picker='year' />
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                <Button type='primary' htmlType='submit'>
+                  Lưu
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab='Chứng nhận, giấy phép' key='9'>
+          <div style={{ padding: '16px 24px 16px 0' }}>
+            <Form {...layout}>
+              <Form.Item label='Chứng nhận' name='certification'>
+                <Input />
+              </Form.Item>
+
+              <Form.Item label='Nơi cấp' name='from'>
+                <Input />
+              </Form.Item>
+
+              <Form.Item label='Năm nhận' name='year'>
+                <DatePicker locale={VN} picker='year' />
               </Form.Item>
 
               <Form.Item {...tailLayout}>
